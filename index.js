2feb62cf996fce841ebd59db84eeaa756f4e4943
TurboTeam335 <105672233+dgmascal@users.noreply.github.com>
2023-05-10 11:35:52 -0500

    TODO list

--- index.js
+++ index.js
@@ -11,10 +11,9 @@ const db = mysql.createConnection({
   database: "employeeTracker_db",
 });
 
-connection.connect(err => {
+db.connect(err => {
   if (err) throw err;
   console.log(`Connected to the employeeTracker_db database.`);
-  start();
 });
 
 const userPrompt = async () => {
@@ -54,41 +53,148 @@ const userPrompt = async () => {
       break;
     case "Update an employee role":
       await updateEmployeeRole();
-      break;
     case "Exit":
-      connection.end();
+      console.log("Goodbye");
+      db.end();
       return;
   }
-};
-
   userPrompt();
-const viewAllDepartments = async () => {
-  const [rows] = await connection.query("SELECT * FROM departments");
-  console.table(rows);
 };
 
-const viewAllRoles = async () => {
-  const [rows] = await connection.query(
-    "SELECT roles.id, roles.title, roles.salary, departments.department_name FROM roles LEFT JOIN departments ON roles.department_id = departments.id"
-  );
-  console.table(rows);
+const start = () => {
+  console.log("Welcome to the Employee Tracker");
+  userPrompt();
 };
 
-const viewAllEmployees = async () => {
-  const [rows] = await connection.query(
-    'SELECT employee.id, employee.first_name, employee.last_name, roles.title, departments.department_name AS department, roles.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id LEFT JOIN employee manager ON employee.manager_id = manager.id'
-  );
-  console.table(rows);
-};
+start();
 
+// TODO Not working at all
+// const viewAllDepartments = async () => {
+//   const [rows] = await db.query("SELECT * FROM departments");
+//   console.table(rows);
+//   userPrompt();
+// };
+
+// const viewAllRoles = async () => {
+//   const [rows] = await db.query(
+//     "SELECT roles.id, roles.title, roles.salary, departments.department_name FROM roles LEFT JOIN departments ON roles.department_id = departments.id"
+//   );
+//   console.table(rows);
+//   userPrompt();
+// };
+
+// const viewAllEmployees = async () => {
+//   const [rows] = await db.query(
+//     'SELECT employee.id, employee.first_name, employee.last_name, roles.title, departments.department_name AS department, roles.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id LEFT JOIN employee manager ON employee.manager_id = manager.id'
+//   );
+//   console.table(rows);
+//   userPrompt();
+// };
+
+//TODO Not working //
 const addDepartment = async () => {
   const { departmentName } = await inquirer.prompt({
     type: "input",
     name: "departmentName",
     message: "What is the name of the department?",
+    validate: input => {
+      if (!input.trim()) {
+        return "Department name cannot be empty.";
+      }
+      return true;
+    },
   });
-  await connection.query(
-    "INSERT INTO departments (department_name) VALUES (?)",
-    [departmentName]
-  );
+  await db.query("INSERT INTO departments (department_name) VALUES (?)", [
+    departmentName.trim(),
+  ]);
+  console.log("Department added successfully!");
+  userPrompt();
+};
+
+const addRole = async () => {
+  const departments = await db.promise().query("SELECT * FROM departments");
+  const { title, salary, departmentId } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of the role?",
+    },
+    {
+      type: "number",
+      name: "salary",
+      message: "What is the salary of the role?",
+      validate: value => {
+        if (value < 0) {
+          return "Please enter a positive number";
+        }
+        return true;
+      },
+    },
+    // TODO Not working either 
+    {
+      type: "list",
+      name: "departmentId",
+      message: "What department does the role belong to?",
+      choices: departments[0].map(({ id, department_name }) => ({
+        name: department_name,
+        value: id,
+      })),
+    },
+  ]);
+  await db
+    .promise()
+    .query(
+      "INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)",
+      [title, salary, departmentId]
+    );
+  console.log(`New role ${title} added successfully`);
+};
+
+const addEmployee = async () => {
+  const roles = await db.promise().query("SELECT id, title FROM roles");
+  const managers = await db
+    .promise()
+    .query(
+      "SELECT id, first_name, last_name FROM employee WHERE manager_id IS NULL"
+    );
+  const { firstName, lastName, roleId, managerId } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "firstName",
+      message: "What is the first name of the employee?",
+    },
+    {
+      type: "input",
+      name: "lastName",
+      message: "What is the last name of the employee?",
+    },
+    {
+      type: "list",
+      name: "departmentId",
+      message: "What is the department?",
+      choices: [
+        { name: "Nuclear Power Plant", value: 1 },
+        { name: "Elementary School", value: 2 },
+        { name: "Krusty Burger", value: 3 },
+      ],
+    },
+
+    // TODO Need help here //
+    {
+      type: "list",
+      name: "managerId",
+      message: "Who is the employee's manager?",
+      choices: managers[0].map(({ id, first_name, last_name }) => ({
+        name: `${first_name} ${last_name}`,
+        value: id,
+      })),
+    },
+  ]);
+  await db
+    .promise()
+    .query(
+      "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)",
+      [firstName, lastName, roleId, managerId]
+    );
+  console.log(`New employee ${firstName} ${lastName} added successfully`);
 };
--- main/db/schema.sql
+++ main/db/schema.sql
@@ -22,8 +22,8 @@ id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT,
-manager_id INT NOT NULL,
-FOREIGN KEY (role_id)
-REFERENCES roles(id)
-ON DELETE SET NULL
+manager_id INT NOT NULL
+-- FOREIGN KEY (role_id)
+-- REFERENCES roles(id)
+-- ON DELETE SET NULL
 );
